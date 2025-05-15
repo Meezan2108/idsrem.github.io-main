51124cf35882ee2c4ffc81743ae5b75df21ab3eb
Meezan_2108 <hamizanalimarjafri2108@gmail.com>
2025-05-15 13:35:34 +0800

    Change the folder name into fasa2(tutup)

---
+++ idsrem.github.io-main/IDSv3.1/listOfQuestions.js
@@ -0,0 +1,199 @@
+export const surveyQuestions = [
+    {
+        id: "introduction",
+        picture: "ids_logo.png",
+        text: "Selamat datang ke kaji selidik bagi 'Pemantauan Dinamika Pembangunan Kerajaan Fasa 2 Bagi Tahun 2025'. Pandangan anda amat penting untuk membantu dan memahami sentimen isu - isu kepimpinan dan pembangunan Negeri Sabah. \n\n\n Hi! Memperkenalkan saya Tuan Awang, mari kita mulakan kaji selidik ini.",
+        options: [{name:"Mula"}]
+    },
+    {
+        id: "zone",
+        text: "Sila pilih 'Zon' anda:",
+        options: [
+            { name: "Kudat", parlimenCodes: ["P167", "P168"] },
+            { name: "Pantai Barat", parlimenCodes: ["P169", "P170", "P171", "P172", "P173", "P174", "P175", "P179"] },
+            { name: "Pendalaman", parlimenCodes: ["P176", "P177", "P178", "P180", "P181", "P182"] },
+            { name: "Sandakan", parlimenCodes: ["P183", "P184", "P185", "P186", "P187"] },
+            { name: "Tawau", parlimenCodes: ["P188", "P189", "P190", "P191"] }
+ 
+        ]
+    },
+    
+    {
+        id: "parlimen",
+        text: "Sila pilih Parlimen anda:",
+        options: [
+            {code: "P167", name: "Kudat", dun: ["N1 Banggi", "N2 Bengkoka", "N3 Pitas", "N4 Tanjong Kapor"]},
+            {code: "P168", name: "Kota Marudu", dun: ["N5 Matunggong", "N6 Bandau", "N7 Tandek"]},
+            {code: "P169", name: "Kota Belud",  dun: ["N8 Pintasan", "N9 Tempasuk", "N10 Usukan", "N11 Kedamaian"]},
+            {code: "P170", name: "Tuaran",  dun: ["N12 Sulaman", "N13 Pantai Dalit", "N14 Tamapruli", "N15 Kiulu"]},
+            {code: "P171", name: "Sepanggar", dun: ["N16 Karambunai", "N17 Darau", "N18 Inanam"]},
+            {code: "P172", name: "Kota Kinabalu",  dun: ["N19 Likas", "N20 Api Api", "N21 Luyang"]},
+            {code: "P173", name: "Putatan",  dun: ["N22 Tanjong Aru", "N23 Petagas", "N24 Tanjung Keramat"]},
+            {code: "P174", name: "Penampang",  dun: ["N25 Kapayan", "N26 Moyog"]},
+            {code: "P175", name: "Papar", dun: ["N27 Limbahau", "N28 Kawang", "N29 Pantai Manis"]},
+            {code: "P176", name: "Kimanis",  dun: ["N30 Bongawan", "N31 Membakut"]},
+            {code: "P177", name: "Beaufort",  dun: ["N32 Kilas", "N33 Kuala Penyu"]},
+            {code: "P178", name: "Sipitang",  dun: ["N34 Lumadan", "N35 Sindumin"]},
+            {code: "P179", name: "Ranau", dun: ["N36 Kundasang", "N37 Karanaan", "N38 Pangitan"]},
+            {code: "P180", name: "Keningau", dun: ["N39 Tambunan", "N40 Bingkor", "N41 Liawan"]},
+            {code: "P181", name: "Tenom",  dun: ["N42 Melalap", "N43 Kemabong"]},
+            {code: "P182", name: "Pensiangan", dun: ["N44 Tulid", "N45 Sook", "N46 Nabawan"]},
+            {code: "P183", name: "Beluran",  dun: ["N47 Telupid", "N48 Sugut", "N49 Labuk"]},
+            {code: "P184", name: "Libaran",  dun: ["N50 Gum - Gum", "N51 Sungai Manila", "N52 Sungai Sibuga"]},  
+            {code: "P185", name: "Batu Sapi",  dun: ["N53 Sekong", "N54 Karamunting"]},
+            {code: "P186", name: "Sandakan",  dun: ["N55 Elopura", "N56 Tanjong Papat"]},
+            {code: "P187", name: "Kinbatangan", dun: ["N57 Kuamut", "N58 Lamag", "N59 Sukau"]},
+            {code: "P188", name: "Lahad Datu",  dun: ["N60 Tungku", "N61 Segama", "N62 Silam", "N63 Kunak"]},
+            {code: "P189", name: "Semporna",  dun: ["N64 Sulabayan", "N65 Senallang", "N66 Bugaya"]}, 
+            {code: "P190", name: "Tawau",  dun: ["N67 Balung", "N68 Apas", "N69 Sri Tanjong"]},
+            {code: "P191", name: "Kelabakan", dun: ["N70 Kukusan", "N71 Tanjung Batu", "N72 Merotai", "N73 Sebatik"]}
+        ]
+    },
+    {
+        id: "jantina",
+        text: "Sila pilih jantina anda:",
+        options: [
+            {name: "Lelaki"},
+            {name: "Perempuan"}
+        ]
+    },
+    {
+        id: "umur",
+        text: "Bagaimana pula umur anda:",
+        options: [
+            {name: "18-25"},
+            {name: "26-35"},
+            {name: "36-45"},
+            {name: "46-55"},
+            {name: "55-65"},
+            {name: "66-75"},
+            {name: "75 & Keatas"},
+        ]
+    },
+    {
+        id: "bangsa",
+        text: "Sila pilih bangsa anda:",
+        options: [
+            {name: "KDMR (MOMOGUN)"},
+            {name: "Bajau (Suluk)"},
+            {name: "Cina"},
+            {name: "Melayu"},
+            {name: "India"},
+            {name: "Lain - Lain"}
+        ]
+    },
+
+    //Additional Question: Dari Mana Sumber Utama after question about 'Banga'. Last updated 20th March 2025
+    {
+        id: "sumberUtama",
+        text: "Dari mana sumber utama anda untuk mendapatkan berita terkini?",
+        options: [
+            {name: "Media Sosial"},
+            {name: "Media Elektronik (contoh: Radio, Televisyen, Internet, dll.)"},
+            {name: "Lingkaran Sosial (contoh: Keluarga, Rakan - Rakan, Komuniti, dll.)"}
+        ]
+    },
+    {
+        id: "pengaruhPersepsi",
+        text: "Faktor manakah yang mempengaruhi persepsi anda terhadap prestasi kerajaan?",
+        options: [
+            {name: "Isu Integriti & Moral"},
+            {name: "Hak - Hak Sabah (MA63)"},
+            {name: "Pembangunan"},
+            {name: "Kepimpinan"},
+            {name: "Lain - Lain"},
+        ]
+    },
+    {
+        id: "beritaTerkini",
+        text: "Bagaimanakah berita terkini mempengaruhi persepsi anda terhadap kerajaan?",
+        options: [
+            {name: "Sangat Positif"},
+            {name: "Agak Positif"},
+            {name: "Neutral"},
+            {name: "Agak Negatif"},
+            {name: "Sangat Negatif"}
+        ]
+    },
+    {
+        id: "faktorLain",
+        text: "Apakah faktor lain yang mempengaruhi anda untuk mengundi?",
+        options: [
+            {name: "Keluarga"},
+            {name: "Rakan"},
+            {name: "Bangsa"},
+            {name: "Hubungan dengan Calon/ADUN/Parti"},
+            {name: "Insentif Pengundi (Voter's Benefits)"},
+            {name: "Pendapat Peribadi"},
+        ]
+    },
+    {
+        id: "partiDanCalon",
+        text: "Adakah anda mengundi berdasarkan Parti atau Calon?",
+        options: [
+            {name: "Parti"},
+            {name: "Calon"}
+        ]
+    },
+    //Additional Question add here : "Adakah anda akan mengundi Adun semasa?". Last updated at 19th March 2025.
+    {
+        id: "mengundiAdun",
+        text: "Adakah anda akan mengundi Adun semasa?",
+        options: [
+            {name: "Ya"},
+            {name: "Tidak"}
+        ]
+
+    },
+    {
+        id: "cenderungUntukMengundi",
+        text: "Merujuk kepada pilihan dinyatakan dibawah, yang manakah lebih cenderung untuk anda undi?",
+        options: [
+            {name: "Parti Nasional"},
+            {name: "Parti Tempatan"},
+            {name: "Tidak Pasti"}
+        ]
+    }, 
+    {
+        id: "partiNasional",
+        text: "Sila pilih jawapan yang berikut:",
+        options: [
+            {name: "Barian Nasional (BN)"},
+            {name: "Pakatan Harapan (PH)"},
+            {name: "Perikatan Nasional (PN)"},
+            {name: "Lain - Lain"}
+        ]
+    },
+    {
+        id: "partiTempatan",
+        text: "Sila pilih jawapan yang berikut:",
+        options: [
+            {name: "PGRS"},
+            {name: "WARISAN"},
+            {name: "PBS"},
+            {name: "STAR"},
+            {name: "UPKO"},
+            {name: "Lain - Lain"}
+        ]
+    },
+    {
+        id: "pemimpinSabah",
+        text: "Akhir sekali, pada pendapat anda siapa yang layak untuk memimpin sabah?",
+        options: [
+            {name: "Hajiji Noor (GRS)"},
+            {name: "Shafie Apdal (WARISAN)"},
+            {name: "Bung Moktar(BN)"},
+            {name: "Ewon Benedick(PH)"},
+            {name: "Lain - Lain"}
+        ]
+    },
+    {
+        id: "isiBorangLagi",
+        text: "Adakah anda ingin mengisi borang lagi sekali?",
+        options: [
+            {name: "Iya, (isi kaji selidik yang baru)"},
+            {name: "Tidak, (Sesi ditamatkan)"},
+
+        ]
+    },
+]
---
+++ idsrem.github.io-main/server/server.js
@@ -0,0 +1,242 @@
+const express = require('express');
+const { Pool } = require('pg');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 10000;
+
+// PostgreSQL configuration
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  //ssl: process.env.DATABASE_SSL === 'true' ? { rejectUnauthorized: false } : false
+  ssl: { rejectUnauthorized: false }
+});
+
+app.use(cors());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+app.post('/export', async (req, res) => {
+  const dataArray = req.body;
+
+  try {
+    // Construct the placeholders for the values
+    const placeholders = dataArray.map((_, index) => `($${index * 40 + 1}, $${index * 40 + 2}, $${index * 40 + 3}, $${index * 40 + 4}, $${index * 40 + 5}, $${index * 40 + 6}, 
+      $${index * 40 + 7}, $${index * 40 + 8}, $${index * 40 + 9}, $${index * 40 + 10}, $${index * 40 + 11}, $${index * 40 + 12}, $${index * 40 + 13}, $${index * 40 + 14}, 
+      $${index * 40 + 15}, $${index * 40 + 16}, $${index * 40 + 17}, $${index * 40 + 18}, $${index * 40 + 19}, $${index * 40 + 20}, $${index * 40 + 21}, $${index * 40 + 22}, 
+      $${index * 40 + 23}, $${index * 40 + 24}, $${index * 40 + 25}, $${index * 40 + 26}, $${index * 40 + 27}, $${index * 40 + 28}, $${index * 40 + 29}, $${index * 40 + 30}, 
+      $${index * 40 + 31}, $${index * 40 + 32}, $${index * 40 + 33}, $${index * 40 + 34}, $${index * 40 + 35}, $${index * 40 + 36}, $${index * 40 + 37}, $${index * 40 + 38}, 
+      $${index * 40 + 39}, $${index * 40 + 40})`).join(',');
+
+    // Flatten the dataArray into a single array of values
+    const values = dataArray.reduce((acc, { 
+      tarikh , dun, umur, jantina, agama, bangsa, tahappendidikan, pekerjaan, pendapatanbulanan, puasdgnpembangunansemasa, yapuasdgnpembangunansemasa, tidakpuasdgnpembangunansemasa, 
+      keperluanasaspuashati, infrastrukturpuashati, kebajikanpuashati, lainlainpuashati, keperluanasastidakpuashati, infrastrukturtidakpuashati, kebajikantidakpuashati, 
+      lainlaintidakpuashati, dunjalanidgnbaik, dunmenyelesaikanmasalah, undidun, cadangancalonyb, kmperubahanpositif, penambahbaikanmasadepan, tiadakesanpositif, perbaikikeperluanasas, 
+      perbaikiinfrastruktur, perbaikiekonomi, perbaikiperkhidmatanawam, perbaikilainlain, tiadapositifkeperluanasas, tiadapositifinfrastruktur, tiadapositifekonomi, 
+      tiadapositifperkhidmatanawam, tiadapositiflainlain, kriteriapemimpinbaik, pilihanpemimpinsabah, pilihanpemimpinsabahlain }) => {
+      
+      acc.push(tarikh, dun, umur, jantina, agama, bangsa, tahappendidikan, pekerjaan, pendapatanbulanan, puasdgnpembangunansemasa, yapuasdgnpembangunansemasa, tidakpuasdgnpembangunansemasa, 
+      keperluanasaspuashati, infrastrukturpuashati, kebajikanpuashati, lainlainpuashati, keperluanasastidakpuashati, infrastrukturtidakpuashati, kebajikantidakpuashati, 
+      lainlaintidakpuashati, dunjalanidgnbaik, dunmenyelesaikanmasalah, undidun, cadangancalonyb, kmperubahanpositif, penambahbaikanmasadepan, tiadakesanpositif, perbaikikeperluanasas, 
+      perbaikiinfrastruktur, perbaikiekonomi, perbaikiperkhidmatanawam, perbaikilainlain, tiadapositifkeperluanasas, tiadapositifinfrastruktur, tiadapositifekonomi, tiadapositifperkhidmatanawam, 
+      tiadapositiflainlain, kriteriapemimpinbaik, pilihanpemimpinsabah, pilihanpemimpinsabahlain || '');
+      return acc;
+    }, []);
+
+    // Construct the SQL query dynamically | table name + column name
+    const queryText = `INSERT INTO idstable (tarikh , dun, umur, jantina, agama, bangsa, tahappendidikan, pekerjaan, pendapatanbulanan, puasdgnpembangunansemasa,
+      yapuasdgnpembangunansemasa, tidakpuasdgnpembangunansemasa, keperluanasaspuashati, infrastrukturpuashati, kebajikanpuashati, lainlainpuashati,
+      keperluanasastidakpuashati, infrastrukturtidakpuashati, kebajikantidakpuashati, lainlaintidakpuashati, dunjalanidgnbaik, dunmenyelesaikanmasalah, undidun, cadangancalonyb,
+      kmperubahanpositif, penambahbaikanmasadepan, tiadakesanpositif, perbaikikeperluanasas, perbaikiinfrastruktur, perbaikiekonomi, 
+      perbaikiperkhidmatanawam, perbaikilainlain, tiadapositifkeperluanasas, tiadapositifinfrastruktur, tiadapositifekonomi, tiadapositifperkhidmatanawam, 
+      tiadapositiflainlain, kriteriapemimpinbaik, pilihanpemimpinsabah, pilihanpemimpinsabahlain) VALUES ${placeholders}`;
+
+    // Execute the query
+    await pool.query(queryText, values);
+    res.status(200).send('Data saved successfully');
+  } catch (error) {
+    console.error('Error saving data', error);
+    res.status(500).send('Error saving data');
+  }
+});
+
+
+//work in progress for cycle 2
+// app.post('/exportResponse', async (req, res) => {
+//   const newDataArray = req.body; // The new data array
+
+//   try {
+//     // Construct placeholders for the new data
+//     // const newPlaceholders = newDataArray.map((_, index) => `($${index * 3 + 1}, $${index * 3 + 2}, $${index * 3 + 3})`).join(',');
+   
+//     const newPlaceholders = newDataArray.map((_, index) => 
+//       // Generate 18 placeholders for each record
+//       `(${
+//         Array.from({ length: 18 }, (_, i) => `$${index * 18 + i + 1}`).join(', ')
+//       })`
+//     ).join(',');
+
+//     // Flatten the new data array into a single array of values
+//     // const newValues = newDataArray.reduce((acc, { field1, field2, field3 }) => {
+//     //   acc.push(field1 || '', field2 || '', field3 || '');  // Push the values for the new table's fields
+//     //   return acc;
+//     // }, []);
+
+//     const values = dataArray.reduce((acc, { 
+//       tarikh , dun, umur, jantina, bangsa, bangsalain,pengaruhmediasemasa,persepsi,persepsilain,
+//       pengaruhberita,faktorlain,pendapatperibadi,partiataucalon,cenderunguntukundi,pilihanpartinasional,pilihanpartitempatan,
+//       pemimpinsabah,pemimpinsabahlain,isiboranglagi}))
+
+//     // Construct the SQL query dynamically for the new table !!change new_table to new table in database
+//     const newQueryText = `INSERT INTO new_table (tarikh , dun, umur, jantina, bangsa, bangsalain,pengaruhmediasemasa,persepsi,persepsilain,
+//       pengaruhberita,faktorlain,pendapatperibadi,partiataucalon,cenderunguntukundi,pilihanpartinasional,pilihanpartitempatan,
+//       pemimpinsabah,pemimpinsabahlain,isiboranglagi) VALUES ${newPlaceholders}`;
+
+//     // Execute the query to insert into the new table
+//     await pool.query(newQueryText, newValues);
+//     res.status(200).send('Data saved successfully to new table');
+//   } catch (error) {
+//     console.error('Error saving data to new table', error);
+//     res.status(500).send('Error saving data to new table');
+//   }
+// });
+
+// new post 
+// app.post('/exportResponse', async (req, res) => {
+//   const userDataArray = req.body;  // The array of user data
+
+//   console.log("Hi Habri")
+//   try {
+//     // Construct the placeholders for the values
+//     const placeholders = userDataArray.map((_, index) => `($${index * 26 + 1}, $${index * 26 + 2}, $${index * 26 + 3}, $${index * 26 + 4}, $${index * 26 + 5}, $${index * 26 + 6}, 
+//       $${index * 26 + 7}, $${index * 26 + 8}, $${index * 26 + 9}, $${index * 26 + 10}, $${index * 26 + 11}, $${index * 26 + 12}, $${index * 26 + 13}, $${index * 26 + 14}, 
+//       $${index * 26 + 15}, $${index * 26 + 16}, $${index * 26 + 17}, $${index * 26 + 18}, $${index * 26 + 19}, $${index * 26 + 20}, $${index * 26 + 21}, $${index * 26 + 22}, 
+//       $${index * 26 + 23}, $${index * 26 + 24}, $${index * 26 + 25}, $${index * 26 + 26})`).join(',');
+
+//     // Flatten the userDataArray into a single array of values
+//     const values = userDataArray.reduce((acc, { 
+//       tarikh, kod, dun, umur, jantina, bangsa, bangsalain, pengaruhmediasemasa, persepsi, persepsilain, pengaruhberita, faktorlain, pendapatperibadi, partiataucalon,
+//       mengundiAdun, tidakundi, cenderunguntukundi, pilihanpartinasional, pilihanpartitempatan, pemimpinsabah, pemimpinsabahlain, isiboranglagi 
+//     }) => {
+//       acc.push(tarikh, kod, dun, umur, jantina, bangsa, bangsalain, pengaruhmediasemasa, persepsi, persepsilain, pengaruhberita, faktorlain, pendapatperibadi, partiataucalon,
+//         mengundiAdun, tidakundi, cenderunguntukundi, pilihanpartinasional, pilihanpartitempatan, pemimpinsabah, pemimpinsabahlain, isiboranglagi || '');
+//       return acc;
+//     }, []);
+
+//     // here
+//     // Construct the SQL query dynamically | table name + column name
+//     const queryText = `INSERT INTO cycle2 (tarikh, kod, dun, umur, jantina, bangsa, bangsalain, pengaruhmediasemasa, persepsi, persepsilain, pengaruhberita, faktorlain,
+//       pendapatperibadi, partiataucalon, mengundiAdun, tidakundi, cenderunguntukundi, pilihanpartinasional, pilihanpartitempatan, pemimpinsabah, pemimpinsabahlain, isiboranglagi) 
+//       VALUES ${placeholders}`;
+
+//     // Execute the query
+//     await pool.query(queryText, values);
+//     res.status(200).send('Data saved successfully');
+//   } catch (error) {
+//     console.error('Error saving data', error);
+//     res.status(500).send('Error saving data');
+//   }
+// });
+
+app.post('/exportResponse', async (req, res) => {
+  const userDataArray = req.body;  // The array of user data
+
+  console.log("Hi Habri");
+  
+  // Ensure the array is valid
+  if (!Array.isArray(userDataArray) || userDataArray.length === 0) {
+    return res.status(400).send('Invalid data');
+  }
+
+  try {
+    // Construct the placeholders for the values dynamically
+    const columnCount = 24;  // Adjust based on the number of columns in your table
+    const placeholders = userDataArray.map((_, index) => {
+      return `(${Array.from({ length: columnCount }, (_, i) => `$${index * columnCount + (i + 1)}`).join(', ')})`;
+    }).join(',');
+
+    // Flatten the userDataArray into a single array of values
+    const values = userDataArray.reduce((acc, {
+      tarikh, kod, dun, umur, jantina, bangsa, bangsalain, pengaruhmediasemasa, persepsi, persepsilain, pengaruhberita, faktorlain, pendapatperibadi, partiataucalon,
+      mengundiAdun, tidakundi, cenderunguntukundi, pilihanpartinasional, pilihanpartitempatan, pemimpinsabah, pemimpinsabahlain, responseid, starttime, endtime
+    }) => {
+      acc.push(tarikh, kod, dun, umur, jantina, bangsa, bangsalain, pengaruhmediasemasa, persepsi, persepsilain, pengaruhberita, faktorlain, pendapatperibadi, partiataucalon,
+        mengundiAdun, tidakundi, cenderunguntukundi, pilihanpartinasional, pilihanpartitempatan, pemimpinsabah, pemimpinsabahlain, responseid, starttime, endtime || '');
+      return acc;
+    }, []);
+
+    // Construct the SQL query dynamically | table name + column name
+    const queryText = `INSERT INTO cycle2 (tarikh, kod, dun, umur, jantina, bangsa, bangsalain, pengaruhmediasemasa, persepsi, persepsilain, pengaruhberita, faktorlain,
+      pendapatperibadi, partiataucalon, mengundiAdun, tidakundi, cenderunguntukundi, pilihanpartinasional, pilihanpartitempatan, pemimpinsabah, pemimpinsabahlain, responseid, starttime, endtime) 
+      VALUES ${placeholders}`;
+
+    // Execute the query
+    await pool.query(queryText, values);
+
+
+   // Send a JSON response
+    res.status(200).json({ message: 'Data saved successfully' });
+  } catch (error) {
+    console.error('Error saving data', error);
+    res.status(500).json({ message: 'Error saving data', error: error.message });
+  }
+});
+
+
+// for test table
+app.post('/testResponse', async (req, res) => {
+  const userDataArray = req.body;  // The array of user data
+
+  console.log("Hi Habri");
+  
+  // Ensure the array is valid
+  if (!Array.isArray(userDataArray) || userDataArray.length === 0) {
+    return res.status(400).send('Invalid data');
+  }
+
+  try {
+    // Construct the placeholders for the values dynamically
+    const columnCount = 24;  // Adjust based on the number of columns in your table
+    const placeholders = userDataArray.map((_, index) => {
+      return `(${Array.from({ length: columnCount }, (_, i) => `$${index * columnCount + (i + 1)}`).join(', ')})`;
+    }).join(',');
+
+    // Flatten the userDataArray into a single array of values
+    const values = userDataArray.reduce((acc, {
+      tarikh, kod, dun, umur, jantina, bangsa, bangsalain, pengaruhmediasemasa, persepsi, persepsilain, pengaruhberita, faktorlain, pendapatperibadi, partiataucalon,
+      mengundiAdun, tidakundi, cenderunguntukundi, pilihanpartinasional, pilihanpartitempatan, pemimpinsabah, pemimpinsabahlain, responseid, starttime, endtime
+    }) => {
+      acc.push(tarikh, kod, dun, umur, jantina, bangsa, bangsalain, pengaruhmediasemasa, persepsi, persepsilain, pengaruhberita, faktorlain, pendapatperibadi, partiataucalon,
+        mengundiAdun, tidakundi, cenderunguntukundi, pilihanpartinasional, pilihanpartitempatan, pemimpinsabah, pemimpinsabahlain, responseid, starttime, endtime || '');
+      return acc;
+    }, []);
+
+    // Construct the SQL query dynamically | table name + column name
+    const queryText = `INSERT INTO cycle2Test (tarikh, kod, dun, umur, jantina, bangsa, bangsalain, pengaruhmediasemasa, persepsi, persepsilain, pengaruhberita, faktorlain,
+      pendapatperibadi, partiataucalon, mengundiAdun, tidakundi, cenderunguntukundi, pilihanpartinasional, pilihanpartitempatan, pemimpinsabah, pemimpinsabahlain, responseid, starttime, endtime) 
+      VALUES ${placeholders}`;
+
+    // Execute the query
+    await pool.query(queryText, values);
+
+
+   // Send a JSON response
+    res.status(200).json({ message: 'Data saved successfully' });
+  } catch (error) {
+    console.error('Error saving data', error);
+    res.status(500).json({ message: 'Error saving data', error: error.message });
+  }
+});
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+
